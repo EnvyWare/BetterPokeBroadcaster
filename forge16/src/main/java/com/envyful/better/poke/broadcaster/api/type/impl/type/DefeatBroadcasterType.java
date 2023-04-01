fce7162480958888f09ce706c1845f33022c3665
poqdavid <poq-david@live.com>
2023-04-01 14:36:59 -0400

    feat(forge16): added more placeholders
    Added some placeholders to the broadcaster. These placeholders can be used to send more information via webhook which can be handy.
    
    %species%, %species_lower%, %level%, %gender%, %unbreedable%, %nature%, %ability%, %untradeable%, %iv_percentage%, %iv_hp%, %iv_attack%, %iv_defence%, %iv_spattack%, %iv_spdefence%, %iv_speed%, %shiny%, %form%, %size%, %custom_texture%

--- forge16/src/main/java/com/envyful/better/poke/broadcaster/api/type/impl/type/DefeatBroadcasterType.java
+++ forge16/src/main/java/com/envyful/better/poke/broadcaster/api/type/impl/type/DefeatBroadcasterType.java
@@ -9,6 +9,9 @@ import com.pixelmonmod.pixelmon.api.util.helpers.BiomeHelper;
 import com.pixelmonmod.pixelmon.battles.controller.participants.BattleParticipant;
 import com.pixelmonmod.pixelmon.battles.controller.participants.WildPixelmonParticipant;
 import com.pixelmonmod.pixelmon.entities.pixelmon.PixelmonEntity;
+import com.pixelmonmod.pixelmon.api.pokemon.Pokemon;
+import com.pixelmonmod.pixelmon.api.pokemon.stats.BattleStatsType;
+import com.pixelmonmod.pixelmon.api.pokemon.stats.IVStore;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -62,12 +65,41 @@ public class DefeatBroadcasterType extends AbstractBroadcasterType<BattleEndEven
 
     @Override
     protected String translateEventMessage(BattleEndEvent event, String line, PixelmonEntity pixelmon, ServerPlayerEntity nearestPlayer) {
+        final Pokemon pokemon = pixelmon.getPokemon();
+        IVStore iVs = pokemon.getIVs();
+        float ivHP = iVs.getStat(BattleStatsType.HP);
+        float ivAtk = iVs.getStat(BattleStatsType.ATTACK);
+        float ivDef = iVs.getStat(BattleStatsType.DEFENSE);
+        float ivSpeed = iVs.getStat(BattleStatsType.SPEED);
+        float ivSAtk = iVs.getStat(BattleStatsType.SPECIAL_ATTACK);
+        float ivSDef = iVs.getStat(BattleStatsType.SPECIAL_DEFENSE);
+        int percentage = Math.round(((ivHP + ivDef + ivAtk + ivSpeed + ivSAtk + ivSDef) / 186f) * 100);
+
         return line.replace("%nearest_name%", nearestPlayer == null ? "None" : nearestPlayer.getName().getString())
                 .replace("%x%", pixelmon.getX() + "")
                 .replace("%y%", pixelmon.getY() + "")
                 .replace("%z%", pixelmon.getZ() + "")
                 .replace("%world%", UtilWorld.getName(pixelmon.level) + "")
                 .replace("%pokemon%", pixelmon.getPokemonName())
+                .replace("%species%", pixelmon.getSpecies().getLocalizedName())
+                .replace("%species_lower%", pixelmon.getSpecies().getLocalizedName().toLowerCase())
+                .replace("%level%", pixelmon.getLvl() + "")
+                .replace("%gender%", pokemon.getGender().getLocalizedName())
+                .replace("%unbreedable%", pokemon.isUnbreedable() ? "True" : "False")
+                .replace("%nature%", pokemon.getNature().getLocalizedName())
+                .replace("%ability%", pokemon.getAbility().getLocalizedName())
+                .replace("%untradeable%", pokemon.isUntradeable() ? "True" : "False")
+                .replace("%iv_percentage%", percentage + "")
+                .replace("%iv_hp%", ((int) ivHP) + "")
+                .replace("%iv_attack%", ((int) ivAtk) + "")
+                .replace("%iv_defence%", ((int) ivDef) + "")
+                .replace("%iv_spattack%", ((int) ivSAtk) + "")
+                .replace("%iv_spdefence%", ((int) ivSDef) + "")
+                .replace("%iv_speed%", ((int) ivSpeed) + "")
+                .replace("%shiny%", pokemon.isShiny() ? "True" : "False")
+                .replace("%form%", pixelmon.getForm().getLocalizedName())
+                .replace("%size%", pokemon.getGrowth().getLocalizedName())
+                .replace("%custom_texture%", pixelmon.getPalette().getLocalizedName())
                 .replace("%biome%", BiomeHelper.getLocalizedBiomeName(pixelmon.level.getBiome(pixelmon.blockPosition())).getString());
     }
 
--- forge16/src/main/java/com/envyful/better/poke/broadcaster/api/type/impl/type/SpawnBroadcasterType.java
+++ forge16/src/main/java/com/envyful/better/poke/broadcaster/api/type/impl/type/SpawnBroadcasterType.java
@@ -6,6 +6,9 @@ import com.envyful.better.poke.broadcaster.api.util.BroadcasterUtil;
 import com.pixelmonmod.pixelmon.api.events.spawning.SpawnEvent;
 import com.pixelmonmod.pixelmon.api.util.helpers.BiomeHelper;
 import com.pixelmonmod.pixelmon.entities.pixelmon.PixelmonEntity;
+import com.pixelmonmod.pixelmon.api.pokemon.Pokemon;
+import com.pixelmonmod.pixelmon.api.pokemon.stats.BattleStatsType;
+import com.pixelmonmod.pixelmon.api.pokemon.stats.IVStore;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraftforge.eventbus.api.EventPriority;
@@ -41,12 +44,41 @@ public class SpawnBroadcasterType extends AbstractBroadcasterType<SpawnEvent> {
 
     @Override
     protected String translateEventMessage(SpawnEvent spawnEvent, String line, PixelmonEntity pixelmon, ServerPlayerEntity nearestPlayer) {
+        final Pokemon pokemon = pixelmon.getPokemon();
+        IVStore iVs = pokemon.getIVs();
+        float ivHP = iVs.getStat(BattleStatsType.HP);
+        float ivAtk = iVs.getStat(BattleStatsType.ATTACK);
+        float ivDef = iVs.getStat(BattleStatsType.DEFENSE);
+        float ivSpeed = iVs.getStat(BattleStatsType.SPEED);
+        float ivSAtk = iVs.getStat(BattleStatsType.SPECIAL_ATTACK);
+        float ivSDef = iVs.getStat(BattleStatsType.SPECIAL_DEFENSE);
+        int percentage = Math.round(((ivHP + ivDef + ivAtk + ivSpeed + ivSAtk + ivSDef) / 186f) * 100);
+
         return line.replace("%nearest_name%", nearestPlayer == null ? "None" : nearestPlayer.getName().getString())
                 .replace("%x%", pixelmon.getX() + "")
                 .replace("%y%", pixelmon.getY() + "")
                 .replace("%z%", pixelmon.getZ() + "")
                 .replace("%world%", UtilWorld.getName(pixelmon.level) + "")
                 .replace("%pokemon%", pixelmon.getPokemonName())
+                .replace("%species%", pixelmon.getSpecies().getLocalizedName())
+                .replace("%species_lower%", pixelmon.getSpecies().getLocalizedName().toLowerCase())
+                .replace("%level%", pixelmon.getLvl() + "")
+                .replace("%gender%", pokemon.getGender().getLocalizedName())
+                .replace("%unbreedable%", pokemon.isUnbreedable() ? "True" : "False")
+                .replace("%nature%", pokemon.getNature().getLocalizedName())
+                .replace("%ability%", pokemon.getAbility().getLocalizedName())
+                .replace("%untradeable%", pokemon.isUntradeable() ? "True" : "False")
+                .replace("%iv_percentage%", percentage + "")
+                .replace("%iv_hp%", ((int) ivHP) + "")
+                .replace("%iv_attack%", ((int) ivAtk) + "")
+                .replace("%iv_defence%", ((int) ivDef) + "")
+                .replace("%iv_spattack%", ((int) ivSAtk) + "")
+                .replace("%iv_spdefence%", ((int) ivSDef) + "")
+                .replace("%iv_speed%", ((int) ivSpeed) + "")
+                .replace("%shiny%", pokemon.isShiny() ? "True" : "False")
+                .replace("%form%", pixelmon.getForm().getLocalizedName())
+                .replace("%size%", pokemon.getGrowth().getLocalizedName())
+                .replace("%custom_texture%", pixelmon.getPalette().getLocalizedName())
                 .replace("%biome%", BiomeHelper.getLocalizedBiomeName(pixelmon.level.getBiome(pixelmon.blockPosition())).getString());
     }
 
